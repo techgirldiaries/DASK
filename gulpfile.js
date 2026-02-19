25e8d3fe455c667cd31b50a7568e357d5760df38
Oluwakemi Obadeyi <contact.oluwakemi@gmail.com>
2026-02-19 22:52:27 +0000

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,140 +1,122 @@
-/* Needed gulp config */
+/* Gulp configuration */
 
-var gulp = require('gulp');  
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var notify = require('gulp-notify');
-var minifycss = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const plumber = require("gulp-plumber");
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer =
+  require("gulp-autoprefixer").default || require("gulp-autoprefixer");
 
-/* Setup scss path */
-var paths = {
-    scss: './sass/*.scss'
-};
-
-/* Scripts task */
-gulp.task('scripts', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'js/vendor/jquery.min.js',
-    'js/vendor/jquery.easing.1.3.js',
-    'js/vendor/jquery.stellar.min.js',
-    'js/vendor/jquery.flexslider-min.js',
-    'js/vendor/imagesloaded.pkgd.min.js',
-    'js/vendor/isotope.pkgd.min.js',
-    'js/vendor/jquery.timepicker.min.js',
-    'js/vendor/bootstrap-datepicker.js',
-    'js/vendor/photoswipe.min.js',
-    'js/vendor/photoswipe-ui-default.min.js',
-    'js/vendor/owl.carousel.min.js',
-    'js/vendor/bootstrap.min.js',
-    'js/vendor/jquery.waypoints.min.js'
+/* Concatenate and minify vendor scripts */
+function scripts() {
+  return gulp
+    .src([
+      "js/jquery.min.js",
+      "js/jquery.easing.1.3.js",
+      "js/jquery.stellar.min.js",
+      "js/jquery.flexslider-min.js",
+      "js/bootstrap.min.js",
+      "js/jquery.waypoints.min.js",
     ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('js'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("js"))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
-});
+    .pipe(gulp.dest("js"));
+}
 
-gulp.task('minify-custom', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'js/custom.js'
-    ])
-    .pipe(rename({suffix: '.min'}))
+/* Minify custom JavaScript */
+function minifyCustom() {
+  return gulp
+    .src("js/custom.js")
+    .pipe(rename({ suffix: ".min" }))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
-});
+    .pipe(gulp.dest("js"));
+}
 
-/* Sass task */
-gulp.task('sass', function () {  
-    gulp.src('scss/style.scss')
+/* Compile SCSS to CSS */
+function sassCompile() {
+  return gulp
+    .src("scss/style.scss")
     .pipe(plumber())
-    .pipe(sass({
-      errLogToConsole: true,
-
-      //outputStyle: 'compressed',
-      // outputStyle: 'compact',
-      // outputStyle: 'nested',
-      outputStyle: 'expanded',
-      precision: 10
-    }))
-
     .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('css'))
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+        precision: 10,
+      }).on("error", sass.logError),
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      }),
+    )
+    .pipe(gulp.dest("css"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("css"))
+    .pipe(reload({ stream: true }));
+}
 
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('css'))
-    /* Reload the browser CSS after every change */
-    .pipe(reload({stream:true}));
-});
+/* Merge vendor CSS files */
+function mergeStyles() {
+  return gulp
+    .src([
+      "css/bootstrap.min.css",
+      "css/animate.css",
+      "css/icomoon.css",
+      "css/flexslider.css",
+      "css/default-skin.css",
+    ])
+    .pipe(concat("styles-merged.css"))
+    .pipe(gulp.dest("css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("css"))
+    .pipe(reload({ stream: true }));
+}
 
-gulp.task('merge-styles', function () {
+/* Reload browser */
+function bsReload(done) {
+  browserSync.reload();
+  done();
+}
 
-    return gulp.src([
-        'css/vendor/bootstrap.min.css',
-        'css/vendor/animate.css',
-        'css/vendor/icomoon.css',
-        'css/vendor/flexslider.css',
-        'css/vendor/owl.carousel.min.css',
-        'css/vendor/owl.theme.default.min.css',
-        'css/vendor/photoswipe.css',
-        'css/vendor/jquery.timepicker.css',
-        'css/vendor/bootstrap-datepicker.css',
-        'css/vendor/default-skin.css',
-        'fonts/icomoon/style.css',
-        ])
-        // .pipe(sourcemaps.init())
-        // .pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: false
-        // }))
-        .pipe(concat('styles-merged.css'))
-        .pipe(gulp.dest('css'))
-        // .pipe(rename({suffix: '.min'}))
-        // .pipe(minifycss())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('css'))
-        .pipe(reload({stream:true}));
-});
+/* Initialize BrowserSync server */
+function browserSyncInit(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+    files: ["css/*.css", "js/*.js"],
+  });
+  done();
+}
 
-/* Reload task */
-gulp.task('bs-reload', function () {
-    browserSync.reload();
-});
+/* Watch files for changes */
+function watchFiles() {
+  gulp.watch(["scss/*.scss", "scss/**/*.scss"], sassCompile);
+  gulp.watch(["js/custom.js"], minifyCustom);
+  gulp.watch(["*.html"], bsReload);
+}
 
-/* Prepare Browser-sync for localhost */
-gulp.task('browser-sync', function() {
-    browserSync.init(['css/*.css', 'js/*.js'], {
-        
-        proxy: 'localhost/probootstrap/resto'
-        /* For a static server you would use this: */
-        /*
-        server: {
-            baseDir: './'
-        }
-        */
-    });
-});
+/* Export tasks */
+exports.scripts = scripts;
+exports.minifyCustom = minifyCustom;
+exports.sass = sassCompile;
+exports.mergeStyles = mergeStyles;
+exports.watch = watchFiles;
 
-/* Watch scss, js and html files, doing different things with each. */
-gulp.task('default', ['sass', 'scripts', 'browser-sync'], function () {
-    /* Watch scss, run the sass task on change. */
-    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass'])
-    /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(['js/custom.js'], ['minify-custom'])
-    /* Watch .html files, run the bs-reload task on change. */
-    gulp.watch(['*.html'], ['bs-reload']);
-});
+/* Default task */
+exports.default = gulp.series(
+  gulp.parallel(sassCompile, scripts),
+  browserSyncInit,
+  watchFiles,
+);
--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,154 @@
 (function() {
 	'use strict';
 
-	/*----------------------------------------
-		Detect Mobile
-	----------------------------------------*/
+	/* Detect Mobile */
 	var isMobile = {
 		Android: function() {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function() {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function() {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function() {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function() {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function() {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
+
+	/* Flexslider Initialization */
+	var flexSlider = function() {
+		jQuery('.flexslider').flexslider({
+			animation: "fade",
+			prevText: "",
+			nextText: "",
+			slideshow: true
+		});
+	};
+
+	/* Animate on Scroll */
+	var contentWayPoint = function() {
+		var i = 0;
+		jQuery('.bootstrap-animate').waypoint( function( direction ) {
+			if( direction === 'down' && !jQuery(this.element).hasClass('bootstrap-animated') ) {
+				i++;
+				jQuery(this.element).addClass('item-animate');
+				setTimeout(function(){
+					jQuery('body .bootstrap-animate.item-animate').each(function(k){
+						var el = jQuery(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn bootstrap-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft bootstrap-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight bootstrap-animated');
+							} else {
+								el.addClass('fadeInUp bootstrap-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 30, 'easeInOutExpo' );
+					});
+				}, 100);
+			}
+		} , { offset: '95%' } );
+	};
+
+	/* Navbar Scroll State */
+	var navbarState = function() {
+		jQuery(window).scroll(function(){
+			var $this = jQuery(this),
+				st = $this.scrollTop();
+
+			if ( st > 5 ) {
+				jQuery('.otl-navbar').addClass('scrolled');
+			} else {
+				jQuery('.otl-navbar').removeClass('scrolled');
+			}
+		});
+	};
+
+	/* Stellar Parallax */
+	var stellarInit = function() {
+		if( !isMobile.any() ) {
+			jQuery(window).stellar();
+		}
+	};
+
+	/* Page Navigation */
+	var clickMenu = function() {
+		jQuery('.navbar-nav a:not([class="external"])').click(function(event){
+			var section = jQuery(this).data('nav-section'),
+				navbar = jQuery('.navbar-nav');
+			
+			if (isMobile.any()) {
+				jQuery('.navbar-toggle').click();
+			}
+			
+			if ( jQuery('[data-section="' + section + '"]').length ) {
+				jQuery('html, body').animate({
+					scrollTop: jQuery('[data-section="' + section + '"]').offset().top - 55
+				}, 500, 'easeInOutExpo');
+			}
+
+			event.preventDefault();
+			return false;
+		});
+	};
+
+	/* Active Navigation State */
+	var navActive = function(section) {
+		var $el = jQuery('.navbar-nav');
+		$el.find('li').removeClass('active');
+		$el.each(function(){
+			jQuery(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+		});
+	};
+
+	/* Navigation Section Tracking */
+	var navigationSection = function() {
+		var $section = jQuery('section[data-section]');
+		
+		$section.waypoint(function(direction) {
+			if (direction === 'down') {
+				navActive(jQuery(this.element).data('section'));
+			}
+		}, {
+			offset: '150px'
+		});
+
+		$section.waypoint(function(direction) {
+			if (direction === 'up') {
+				navActive(jQuery(this.element).data('section'));
+			}
+		}, {
+			offset: function() { return -jQuery(this.element).height() - 155; }
+		});
+	};
+
+	/* Initialize on DOM Ready */
+	jQuery(function(){
+		contentWayPoint();
+		navbarState();
+		stellarInit();
+		clickMenu();
+		navigationSection();
+	});
+
+	/* Initialize on Window Load */
+	jQuery(window).load(function(){
+		flexSlider();
+	});
+
+})();
 
 	/*----------------------------------------
 		Carousel
